5cbae49ddf561312440a00cfb0895eb8d207c569
cheng <berialcheng@gmail.com>
2013-04-16 23:38:57 +0800

    add project jacoco

---
+++ jacoco/child1/src/main/java/org/nirvana/Main.java
@@ -0,0 +1,13 @@
+package org.nirvana;
+
+public class Main {
+
+	public void methodOne() {
+		System.out.println("method one");
+	}
+
+	public void methodTwo() {
+		System.out.println("method two");
+	}
+
+}
