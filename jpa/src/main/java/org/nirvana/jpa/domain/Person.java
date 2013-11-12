83518f2d59be2d1daf014de540eae985243538d3
berialcheng <berialcheng@gmail.com>
2013-11-12 17:25:26 +0800

    jpa project change

--- jpa/src/main/java/org/nirvana/jpa/domain/Person.java
+++ jpa/src/main/java/org/nirvana/jpa/domain/Person.java
@@ -1,75 +1,87 @@
 package org.nirvana.jpa.domain;
 
 import java.io.Serializable;
+import java.util.Collection;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "PERSON")
-public class Person implements Serializable{
-	
-	private static final long serialVersionUID = 1L;
+public class Person implements Serializable
+{
 
-	@Id 
-	@GeneratedValue
-	@Column(name = "ID")
-	private String id;
-	
-	@Column(name = "FIRST_NAME")
-	private String firstName;
-	
-	@Column(name = "LAST_NAME")
-	private String lastName;
-	
-	@Column(name = "PHONE")
-	private String phone;
-	
-	public Person() {
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID ")
+    private int id;
 
-	public Person(String firstName, String lastName, String phone) {
-		super();
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.phone = phone;
-	}
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+
+    @Column(name = "LAST_NAME")
+    private String lastName;
+
+    @OneToMany(cascade = {CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH, CascadeType.REMOVE}, mappedBy = "person", fetch = FetchType.LAZY)
+    private Collection<Phone> phones;
+
+    public Person()
+    {
+    }
+
+    public Person(String firstName, String lastName)
+    {
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Collection<Phone> getPhones()
+    {
+        return phones;
+    }
+
+    public void setPhones(Collection<Phone> phones)
+    {
+        this.phones = phones;
+    }
 
 
-	public String getFirstName() {
-		return firstName;
-	}
+    public String getFirstName()
+    {
+        return firstName;
+    }
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
 
-	public String getLastName() {
-		return lastName;
-	}
+    public String getLastName()
+    {
+        return lastName;
+    }
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
 
-	public String getPhone() {
-		return phone;
-	}
+    public int getId()
+    {
+        return id;
+    }
 
-	public String getId() {
-		return id;
-	}
+    public void setId(int id)
+    {
+        this.id = id;
+    }
 
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-
-	
 }
---
+++ jpa/src/main/java/org/nirvana/jpa/domain/Phone.java
@@ -0,0 +1,56 @@
+package org.nirvana.jpa.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "PHONE")
+public class Phone implements Serializable
+{
+    @Id
+    @Column(name = "NUMBER")
+    private String number;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "PERSON_ID")
+    private Person person;
+
+    public String getNumber()
+    {
+        return number;
+    }
+
+    public Phone(String number, Person person)
+    {
+        super();
+        this.number = number;
+        this.person = person;
+    }
+
+    public Phone()
+    {
+        // TODO Auto-generated constructor stub
+    }
+
+    public Person getPerson()
+    {
+        return person;
+    }
+
+    public void setPerson(Person person)
+    {
+        this.person = person;
+    }
+
+    public void setNumber(String number)
+    {
+        this.number = number;
+    }
+}
