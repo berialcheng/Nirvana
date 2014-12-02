ef30c7775dbe35feea62ee1e5986c1cc49d3da82
Cheng.Zhong <czhong@hp.com>
2014-12-02 14:39:15 +0800

    format main in mongodb

--- mongodb/java/src/main/java/Main.java
+++ mongodb/java/src/main/java/Main.java
@@ -10,49 +10,42 @@ import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.WriteResult;
 
-public class Main
-{
+public class Main {
 
-    /**
-     * @param args
-     * @throws Exception 
-     */
-    public static void main(String[] args)
-        throws Exception
-    {
-        //init
-        MongoClient mongoClient = new MongoClient();
-        DB db = mongoClient.getDB("argus");
+	/**
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+		// init
+		MongoClient mongoClient = new MongoClient();
+		DB db = mongoClient.getDB("argus");
 
-        //Count
-        DBCollection dbcollection = db.getCollection("notification.email");
-        System.out.println(dbcollection.getCount());
+		// Count
+		DBCollection dbcollection = db.getCollection("notification.email");
+		System.out.println(dbcollection.getCount());
 
-        //Iterator
-        DBCursor all = dbcollection.find();
-        try
-        {
-            while (all.hasNext())
-            {
-                System.out.println(all.next());
-            }
-        }
-        finally
-        {
-            all.close();
-        }
+		// Iterator
+		DBCursor all = dbcollection.find();
+		try {
+			while (all.hasNext()) {
+				System.out.println(all.next());
+			}
+		} finally {
+			all.close();
+		}
 
-        DBObject filter = new BasicDBObject("_id", new ObjectId("51467751d51adb62e990ed00"));
-        DBObject update = new BasicDBObject("$set", new BasicDBObject().append("sendTs",
-                new Timestamp(System.currentTimeMillis())));
+		DBObject filter = new BasicDBObject("_id", new ObjectId(
+				"51467751d51adb62e990ed00"));
+		DBObject update = new BasicDBObject("$set", new BasicDBObject().append(
+				"sendTs", new Timestamp(System.currentTimeMillis())));
 
-        DBCursor cursor = dbcollection.find(filter);
-        while (cursor.hasNext())
-        {
-            System.out.print(cursor.next());
-        }
-        WriteResult result = dbcollection.update(filter, update);
-        System.out.println();
-    }
+		DBCursor cursor = dbcollection.find(filter);
+		while (cursor.hasNext()) {
+			System.out.print(cursor.next());
+		}
+		WriteResult result = dbcollection.update(filter, update);
+		System.out.println();
+	}
 
 }
